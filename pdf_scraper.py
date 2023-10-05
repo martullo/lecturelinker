bd9a9cafd7df6633039ed19355ebcf9ad2ed8b60
martullo <mleutwyler@student.ethz.ch>
2023-10-05 22:05:37 +0200

    Modified pdf_scraper.py

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -4,7 +4,6 @@ import time
 
 def get_list(url):
     start = time.time()
-    #browser shouldn't be opened and closed in this function
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
@@ -15,16 +14,13 @@ def get_list(url):
         page.goto(url)
         print(time.time()-start)
 
-        # page.wait not needed since most javascript which loads data runs onload
-        # page.wait_for_timeout(10)
-
         pdf_links = []
 
         for link in page.eval_on_selector_all('a', 'elements => elements.map(el => el.href)'):
             if link.endswith('.pdf'):
                 pdf_links.append(link)
                 print(link)
-        
+
         browser.close()
 
         return pdf_links
